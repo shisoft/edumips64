baee08cd29d905b9dd50101cca6621cc81181eb9
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2016-07-03 20:58:06 +0100

    Temporary work for JS API
    - start implementing the API in WebUI
    - first steps using the new API in utils.html

--- src/main/java/org/edumips64/client/WebUi.java
+++ src/main/java/org/edumips64/client/WebUi.java
@@ -4,11 +4,27 @@ import com.google.gwt.core.client.EntryPoint;
 
 import jsinterop.annotations.JsType;
 
-import org.edumips64.core.*;
+import org.edumips64.core.CPU;
+import org.edumips64.core.IrregularWriteOperationException;
+import org.edumips64.core.Memory;
+import org.edumips64.core.MemoryElementNotFoundException;
+import org.edumips64.core.NotAlignException;
+import org.edumips64.core.Parser;
+import org.edumips64.core.ParserMultiException;
+import org.edumips64.core.Register;
+import org.edumips64.core.StoppedCPUException;
+import org.edumips64.core.SynchronousException;
+import org.edumips64.core.fpu.FPDividerNotAvailableException;
+import org.edumips64.core.fpu.FPFunctionalUnitNotAvailableException;
+import org.edumips64.core.fpu.MemoryNotAvailableException;
+import org.edumips64.core.is.AddressErrorException;
+import org.edumips64.core.is.BreakException;
 import org.edumips64.core.is.HaltException;
-import org.edumips64.core.is.InstructionBuilder;
+import org.edumips64.core.is.TwosComplementSumException;
+import org.edumips64.core.is.WAWException;
 import org.edumips64.utils.ConfigStore;
 import org.edumips64.utils.ConfigManager;
+import org.edumips64.utils.IrregularStringOfBitsException;
 import org.edumips64.utils.io.FileUtils;
 import org.edumips64.utils.io.NullFileUtils;
 
@@ -25,6 +41,49 @@ public class WebUi implements EntryPoint {
   private InstructionBuilder instructionBuilder;
   private IOManager iom;
   private Dinero dinero;
+
+  class WrongStateException extends Exception {
+    WrongStateException(State expected, State actual) {
+      super("Expected state " + expected.name() + ", got " + actual.name());
+    }
+
+    WrongStateException() {
+    }
+  }
+
+  enum State {
+    // Simulator just loaded, nothing parsed.
+    IDLE,
+    // Program is running.
+    RUNNING,
+    // The program finished running
+    HALTED
+  };
+  private State state;
+
+  private void checkState(State expected) throws WrongStateException {
+    if (expected != state) {
+      throw new WrongStateException(expected, state);
+    }
+  }
+
+  /** Will transition from IDLE to READY */
+  public void loadProgram(String code) throws WrongStateException, ParserMultiException {
+    checkState(State.IDLE);
+    parser.doParsing(code);
+    cpu.setStatus(CPU.CPUStatus.RUNNING);
+    state = State.RUNNING;
+  }
+
+  /** This can throw a ton of exceptions.. need to be handled differently in the simulator */
+  public void step() throws WrongStateException, MemoryNotAvailableException, NotAlignException, WAWException, SynchronousException, IrregularStringOfBitsException, StoppedCPUException, BreakException, FPFunctionalUnitNotAvailableException, HaltException, FPDividerNotAvailableException, IrregularWriteOperationException, AddressErrorException, MemoryElementNotFoundException, TwosComplementSumException {
+    checkState(State.RUNNING);
+    try {
+      cpu.step();
+    } catch (HaltException e) {
+      state = State.HALTED;
+    }
+  }
 
   // Executes the program. Returns an empty string on success, or an error message.
   public String runProgram(String code) {
@@ -50,11 +109,24 @@ public class WebUi implements EntryPoint {
     }
   }
 
-  public String getMemory() {
-    return memory.toString();
+  public Memory getMemory() {
+    return cpu.getMemory();
   }
 
-  public String getRegisters() {
+  public Register[] getRegisters() {
+    return cpu.getRegisters();
+  }
+
+  public void reset() {
+    state = State.IDLE;
+    cpu.reset();
+  }
+
+  public String getMemoryString() {
+    return cpu.getMemory().toString();
+  }
+
+  public String getRegistersString() {
     return cpu.gprString();
   }
 
@@ -84,5 +156,6 @@ public class WebUi implements EntryPoint {
     dinero = new Dinero(memory);
     instructionBuilder = new InstructionBuilder(memory, iom, cpu, dinero);
     parser = new Parser(fu, symTab, memory, instructionBuilder);
+    state = State.IDLE;
   }
 }
